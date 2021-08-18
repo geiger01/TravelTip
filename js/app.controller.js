5bc3ee5783423746e6140b610431bee4e5899b1c
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-18 18:08:37 +0300

    add func

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,63 +1,64 @@
 import { locService } from './services/loc.service.js';
 import { mapService } from './services/map.service.js';
 
-
 window.onload = onInit;
 window.onAddMarker = onAddMarker;
 window.onPanTo = onPanTo;
 window.onGetLocs = onGetLocs;
 window.onGetUserPos = onGetUserPos;
-
+window.onAddLocation = onAddLocation;
 
 function onInit() {
-    mapService
-        .initMap()
-        .then(() => {
-            console.log('Map is ready');
-        })
-        .catch(() => console.log('Error: cannot init map'));
+  mapService
+    .initMap()
+    .then(() => {
+      console.log('Map is ready');
+    })
+    .catch(() => console.log('Error: cannot init map'));
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos');
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+  console.log('Getting Pos');
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 }
 
 function onAddMarker() {
-    console.log('Adding a marker');
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+  console.log('Adding a marker');
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
 }
 
 function onGetLocs() {
-    locService.getLocs()
-        .then(locs => {
-            console.log('Locations:', locs)
-            document.querySelector('.locs').innerText = JSON.stringify(locs)
-
-        })
+  locService.getLocs().then((locs) => {
+    console.log('Locations:', locs);
+    document.querySelector('.locs').innerText = JSON.stringify(locs);
+  });
 }
 
 function onGetUserPos() {
-    getPosition()
-        .then(pos => {
-            console.log('User position is:', pos.coords);
-            document.querySelector('.user-pos').innerText =
-                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-            mapService.panTo(pos.coords.latitude, pos.coords.longitude)
-            const loc = { lat: pos.coords.latitude, lng: pos.coords.longitude }
-            mapService.addMarker(loc)
-        })
-        .catch(err => {
-            console.log('err!!!', err);
-        })
+  getPosition()
+    .then((pos) => {
+      console.log('User position is:', pos.coords);
+      document.querySelector(
+        '.user-pos'
+      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`;
+      mapService.panTo(pos.coords.latitude, pos.coords.longitude);
+      const loc = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+      mapService.addMarker(loc);
+    })
+    .catch((err) => {
+      console.log('err!!!', err);
+    });
 }
 
 function onPanTo() {
-    const searchLocation = document.querySelector('.location-input').value;
-    console.log('Panning the Map');
-    mapService.codeAddress(searchLocation, mapService.panTo)
+  const searchLocation = document.querySelector('.location-input').value;
+  console.log('Panning the Map');
+  mapService.codeAddress(searchLocation, mapService.panTo);
 }
 
+function onAddLocation() {
+  console.log('dsa');
+}
